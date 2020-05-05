d026694cbe944d33442d6540d6049a0fb3b89863
Railson Rames <rames.dev@outlook.com>
2020-05-05 11:48:12 -0300

    WIP - Dynamic post.

--- JuliusCaesarCryptographyChallengeCodenation/Controllers/ChallengeMessageController.cs
+++ JuliusCaesarCryptographyChallengeCodenation/Controllers/ChallengeMessageController.cs
@@ -47,5 +47,46 @@ namespace JuliusCaesarCryptographyChallengeCodenation.Controllers
 
             return enigmaSolved;
         }
+
+        [HttpPost("dynamic")]
+        public EnigmaModel PostDynamicDecrypted([FromBody] DynamicModel model)
+        {
+            if (string.IsNullOrEmpty(model.UrlRequest))
+                model.UrlRequest = "https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token=";
+
+            if (string.IsNullOrEmpty(model.Token))
+                model.Token = "3b87f90669afa2dc83e82e1b3ae7be47a40f24b8";
+
+            if (string.IsNullOrEmpty(model.UrlResponse))
+                model.UrlResponse = "https://api.codenation.dev/v1/challenge/dev-ps/submit-solution?token=";
+
+            var requestUrl = WebRequest.CreateHttp(model.UrlRequest + model.Token);
+            requestUrl.Method = "GET";
+
+            using var response = requestUrl.GetResponse();
+            var stream = response.GetResponseStream();
+            var reader = new StreamReader(stream ?? throw new InvalidOperationException());
+            var result = reader.ReadToEnd();
+
+            stream.Close();
+            response.Close();
+
+            var options = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                WriteIndented = true
+            };
+
+            var enigmaModel = JsonSerializer.Deserialize<EnigmaModel>(result, options);
+
+            var enigmaSolved = new MessageDecrypter().Decripter(enigmaModel);
+
+            var responseUrl = WebRequest.CreateHttp(model.UrlResponse + model.Token);
+            responseUrl.Method = "POST";
+
+
+
+            return enigmaSolved;
+        }
     }
 }
---
+++ JuliusCaesarCryptographyChallengeCodenation/Models/DynamicModel.cs
@@ -0,0 +1,9 @@
+﻿namespace JuliusCaesarCryptographyChallengeCodenation.Models
+{
+    public class DynamicModel
+    {
+        public string UrlRequest { get; set; }
+        public string Token { get; set; }
+        public string UrlResponse { get; set; }
+    }
+}
